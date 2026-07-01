2fd66e24390b5daa8be0b291a6151e3cb5525adc
donnan gravelle <donnan@MacBook-Pro.local>
2026-07-01 15:30:27 -0400

    Added extract_study_level_effects and fixed BayesForest function

--- R/add_probability_statements.R
+++ R/add_probability_statements.R
@@ -12,7 +12,6 @@
 #' add_probability_statements(model_string, prob_conditions, predictor_vars)
 #' }
 
-
 add_probability_statements <- function(model_string, prob_conditions, predictor_vars) {
   for (param in names(prob_conditions)) {
     conditions <- prob_conditions[[param]]
